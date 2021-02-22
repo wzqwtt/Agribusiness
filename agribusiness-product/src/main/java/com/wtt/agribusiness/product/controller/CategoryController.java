3fe1a1d694c8f2946ea41e4948fc4ad0f027782c
wzqwzq <1722249371@qq.com>
2021-02-22 13:10:02 +0800

    添加分类

--- agribusiness-product/src/main/java/com/wtt/agribusiness/product/controller/CategoryController.java
+++ agribusiness-product/src/main/java/com/wtt/agribusiness/product/controller/CategoryController.java
@@ -52,7 +52,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
