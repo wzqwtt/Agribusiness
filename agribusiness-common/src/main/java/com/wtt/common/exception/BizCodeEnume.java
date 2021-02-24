19526c8c85af8896ded77b9317915db5de7de89f
wzqwzq <1722249371@qq.com>
2021-02-24 16:26:58 +0800

    枚举类封装异常的状态码和错误信息

---
+++ agribusiness-common/src/main/java/com/wtt/common/exception/BizCodeEnume.java
@@ -0,0 +1,22 @@
+package com.wtt.common.exception;
+
+public enum BizCodeEnume {
+    UNKNOW_EXCEPTION(10000,"系统未知异常"),
+    VAILD_EXCEPTION(10001,"参数格式校验失败");
+
+    private int code;
+    private String msg;
+
+    BizCodeEnume(int code,String msg){
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
