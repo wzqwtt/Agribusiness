916d2a60a89b6ae6509994b34ebb83624b79165b
wzqwzq <1722249371@qq.com>
2021-03-03 16:54:15 +0800

    仓库维护模块模糊查询

--- agribusiness-ware/src/main/java/com/wtt/agribusiness/ware/service/impl/WareInfoServiceImpl.java
+++ agribusiness-ware/src/main/java/com/wtt/agribusiness/ware/service/impl/WareInfoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wtt.agribusiness.ware.service.impl;
 
+import com.mysql.cj.util.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,19 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareInfoEntity> wrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if(!StringUtils.isNullOrEmpty(key)){
+            wrapper.eq("id",key)
+                    .or().like("name",key)
+                    .or().like("address",key)
+                    .or().like("areacode",key);
+        }
+
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
