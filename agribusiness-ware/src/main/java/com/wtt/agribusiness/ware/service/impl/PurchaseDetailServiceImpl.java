0255d485c138590d39f26ef7f52d2f21897206a5
wzqwzq <1722249371@qq.com>
2021-03-03 17:15:38 +0800

    仓库采购模块模糊查询

--- agribusiness-ware/src/main/java/com/wtt/agribusiness/ware/service/impl/PurchaseDetailServiceImpl.java
+++ agribusiness-ware/src/main/java/com/wtt/agribusiness/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wtt.agribusiness.ware.service.impl;
 
+import com.mysql.cj.util.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,31 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        /**
+         * status:0 //状态
+         * wareId:1 //仓库id
+         */
+        QueryWrapper<PurchaseDetailEntity> wrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if(!StringUtils.isNullOrEmpty(key)){
+            wrapper.and(item->{
+                item.eq("purchase_id",key).or().eq("sku_id",key);
+            });
+        }
+        String status = (String) params.get("status");
+        if(!StringUtils.isNullOrEmpty(status)){
+            wrapper.eq("status",status);
+        }
+        String wareId = (String) params.get("wareId");
+        if(!StringUtils.isNullOrEmpty(wareId)){
+            wrapper.eq("ware_id",wareId);
+        }
+
         IPage<PurchaseDetailEntity> page = this.page(
                 new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
--- agribusiness-ware/src/main/java/com/wtt/agribusiness/ware/service/impl/WareSkuServiceImpl.java
+++ agribusiness-ware/src/main/java/com/wtt/agribusiness/ware/service/impl/WareSkuServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wtt.agribusiness.ware.service.impl;
 
+import com.mysql.cj.util.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,24 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> wrapper = new QueryWrapper<>();
+        /**
+         * skuId : 1
+         * wareId : 2
+         */
+        String skuId = (String) params.get("skuId");
+        if(!StringUtils.isNullOrEmpty(skuId)){
+            wrapper.eq("sku_id",skuId);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if(!StringUtils.isNullOrEmpty(wareId)){
+            wrapper.eq("ware_id",wareId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
