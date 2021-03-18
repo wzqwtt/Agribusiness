721bc521d63deaffb66e5be918d31c64d50effab
wzqwzq <1722249371@qq.com>
2021-03-18 20:58:07 +0800

    购物车服务环境搭建

---
+++ agribusiness-cart/src/test/java/com/wtt/agribusiness/cart/AgribusinessCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtt.agribusiness.cart;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AgribusinessCartApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
