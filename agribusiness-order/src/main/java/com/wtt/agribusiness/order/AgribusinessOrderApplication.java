18ccbecd685c4fe93a719046a951931f2c603d0a
wzqwzq <1722249371@qq.com>
2021-03-22 15:22:52 +0800

    整合RabbitMQ

--- agribusiness-order/src/main/java/com/wtt/agribusiness/order/AgribusinessOrderApplication.java
+++ agribusiness-order/src/main/java/com/wtt/agribusiness/order/AgribusinessOrderApplication.java
@@ -1,10 +1,12 @@
 package com.wtt.agribusiness.order;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableRabbit
 @EnableDiscoveryClient
 @MapperScan("com.wtt.agribusiness.order.dao")
 @SpringBootApplication
--- agribusiness-order/src/test/java/com/wtt/agribusiness/order/AgribusinessOrderApplicationTests.java
+++ agribusiness-order/src/test/java/com/wtt/agribusiness/order/AgribusinessOrderApplicationTests.java
@@ -1,13 +1,28 @@
 package com.wtt.agribusiness.order;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AgribusinessOrderApplicationTests {
+public class AgribusinessOrderApplicationTests {
 
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
+    /**
+     * 1、如何创建Exchange、Queue、Binding
+     * 2、如何接收消息
+     */
     @Test
-    void contextLoads() {
+    public void createExchange() {
+//        amqpAdmin
     }
+
+
 
 }
